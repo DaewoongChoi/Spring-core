4c6cb8823fc076952e608d780fb3e46678adeae6
Choi-chunsam <cdw5713@naver.com>
2023-11-05 22:08:02 +0900

    롬복 추가

--- src/test/java/hello/core/AutoAppConfigTest.java
+++ src/test/java/hello/core/AutoAppConfigTest.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,6 +9,10 @@ public class AutoAppConfigTest {
 
     @Test
     void basicScan() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext()
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        System.out.println(ac.getBean("memoryMemberRepository", MemoryMemberRepository.class));
+
+
     }
 }
